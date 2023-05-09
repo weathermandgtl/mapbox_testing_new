eb65aa1410d147f3120b27a625ee86adfb188f46
weathermandgtl <120067549+weathermandgtl@users.noreply.github.com>
2023-05-09 16:31:44 -0400

    uncomment fiona

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ if os.path.exists(local_filename):
 
 # # import convert
 # # os.system('pip install fiona')
-# os.system('fio cat current_all/current_all.shp > current_all.geojson.ld')
-# print(f'Converted {time.perf_counter() - start}')
+os.system('fio cat current_all/current_all.shp > current_all.geojson.ld')
+print(f'Converted {time.perf_counter() - start}')
 
 # os.system('pip install mapbox-tilesets')
 os.system(f'tilesets upload-source joshphillips43 current_all current_all.geojson.ld --replace --token {token}')
